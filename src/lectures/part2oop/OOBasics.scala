3add44fe14027d7c4ac7e52814c3a1500f2b1909
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2019-12-08 11:20:00 +0100

    [IMP] OOP Functional

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -7,11 +7,26 @@ object OOBasics extends App {
   person.greet("Daniel")
   person.greet()
 
+  println("Exercise\n==============")
+  val author = new Writer("Charles", "Dickens", 1812)
+  val impostor = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
+
+  println(novel.authorAge)
+  println(novel.isWrittenBy(author))
+  println(novel.isWrittenBy(impostor))
+
+  val counter = new Counter
+  counter.increment.print
+  counter.increment.increment.increment.print
+  counter.increment(10).print
+
 }
 
 //constructor
 class Person(name: String, val age: Int = 0){
-
+  // class parameters are NOT FIELDS
+  // use val to add that value
   // body
   val x = 2 // MEMBER
 
@@ -30,7 +45,55 @@ class Person(name: String, val age: Int = 0){
 }
 
 
+/*
+Novel and Writer class
 
-// class parameters are NOT FIELDS
+  Writer: first name, surname, year
+    - method fullname
 
-// use val to add that value
+ Novel: name, year of release, author
+    - authorAge
+    - isWrittenBy(author)
+    - copy (new year of release) = new instance of Novel
+ */
+
+
+/*
+  Counter class
+    - receives an int value
+    - method current count
+    - method to increment/decrement => new Counter
+    - overload inc/dec to receive an amount
+ */
+
+class Writer(val firstName: String, val surname: String, val year: Int){
+  def fullName: String = s"$firstName $surname"
+}
+
+class Novel(val name: String, val year: Int, val author: Writer){
+  def authorAge: Int = this.year - this.author.year
+  def isWrittenBy(author: Writer): Boolean = author == this.author
+  def copy(newYear: Int): Novel = new Novel(this.name, newYear, this.author)
+}
+
+class Counter(val value: Int = 0 ){
+  def currentCount(): Int = this.value // could be omitted
+  def increment: Counter = {
+    println("Incrementing...")
+    new Counter(this.value + 1) // immutability
+  }
+  def decrement: Counter = {
+    println("Decrementing...")
+    new Counter(this.value - 1)
+  }
+  def increment(n: Int): Counter =
+    if (n <= 0 ) this
+    else increment.increment(n-1)
+
+  def decrement(n: Int): Counter =
+    if (n <= 0) this
+    else decrement.decrement(n-1)
+
+  def print = println(value)
+
+}
