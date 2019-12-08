ff34f1dea7b98917e756af3ceb1349bc68549859
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2019-12-08 10:35:45 +0100

    [IMP] update lecture

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -2,10 +2,35 @@ package lectures.part2oop
 
 object OOBasics extends App {
 
-  val person = new Person
-  println(person)
+  val person = new Person("Raúl", 34)
+  println(person.x)
+  person.greet("Daniel")
+  person.greet()
 
 }
 
+//constructor
+class Person(name: String, val age: Int = 0){
 
-class Person
+  // body
+  val x = 2 // MEMBER
+
+  println(1+3) // BLOCK CODES WILL BE EVALUATED DURING INSTANTIATION
+
+  def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
+
+  // Overloading
+  def greet(): Unit = println(s"Hi, I am $name")
+
+  // multiple constructors
+  // auxiliary constructors calling the base
+  def this(name: String) = this(name, 0)
+
+  def this() = this("John Doe")
+}
+
+
+
+// class parameters are NOT FIELDS
+
+// use val to add that value
