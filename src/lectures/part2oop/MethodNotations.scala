46910c9174fa02d81a172ce731c1acf9a3a03db5
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2019-12-22 22:34:10 +0100

    [IMP] update

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -5,19 +5,39 @@ object MethodNotations extends App {
   class Person(val name: String, favoriteMovie: String){
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def unary_! : String = s"$name, what the heck!!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is ${name} and I like ${favoriteMovie}"
   }
 
   val mary = new Person("Mary", "Inception")
   println(mary.likes("Inception"))
   println(mary likes "Inception") // equivalent
-  // infix notation == operator notation --> only with methods with 1 argument
+  // Infix notation
+
+  //   operator notation --> only with methods with 1 argument
+  // syntatic sugar
 
   // "operators" in scala
   val tom = new Person("Tom", "Fight Club")
   println(mary + tom)
+
+  // All operators are methods
   println(1 + 2)
   println(1.+(2))
 
-  // All operators are methods
+  // Prefix notation
+  val x = -1
+  val y = 1.unary_-
 
+  println(!mary)
+  println(mary.unary_!)
+
+  // Postfix notation (only for methods without parameters)
+  println(mary.isAlive)
+  //println(mary isAlive)
+
+  // Apply
+  println(mary.apply())
+  println(mary()) // equivalent
 }
