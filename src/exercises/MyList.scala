88a66f448f3b477f897251abda6ac4931c079e46
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2020-02-18 08:47:57 +0100

    [IMP] OO expanding

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -22,6 +22,12 @@ abstract class MyList[+A]{
   def printElements: String
   override def toString: String = "["+ printElements +"]"
 
+  def map[B](transformer: MyTransformer[A,B]): MyList[B]
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  def ++[B >: A](list: MyList[B]): MyList[B]
+
 }
 
 object Empty extends MyList[Nothing] {
@@ -30,6 +36,12 @@ object Empty extends MyList[Nothing] {
   def isEmpty: Boolean = true
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   override def printElements: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing,B]): MyList[B] = Empty
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def ++ [B >: Nothing](list: MyList[B]): MyList[B] = list
+
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -41,6 +53,29 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   def printElements: String =
     if(t.isEmpty) "" + h
     else h + " " + t.printElements
+
+  def map[B](transformer: MyTransformer[A,B]): MyList[B] =
+    new Cons(transformer.transform(h), t.map(transformer))
+
+
+  def ++ [B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(head)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+
+
+}
+trait MyPredicate[-T] {
+  def test(elem: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(elem: A): B
 }
 
 object ListTest extends App {
@@ -49,6 +84,14 @@ object ListTest extends App {
 
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
+
+  val anotherListOfIntegers : MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+
+  println((listOfIntegers ++ anotherListOfIntegers))
+  println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]]{
+    override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons(elem + 1, Empty))
+  }).toString)
+
 }
 
 
